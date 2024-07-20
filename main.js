dc8ace6e990798ddd1e396d36e555b50642e1cca
Elizaveta <lisanasluzova@gmail.com>
2024-07-20 19:43:06 +0200

    task from notion

--- main.js
+++ main.js
@@ -30,3 +30,46 @@ function extractCurrencyValue(str) {
 extractCurrencyValue("$120");
 newValue = value;
 console.log(isFinite(newValue));
+
+function showVerticalMessage(message) {
+  if (message.length <= 10) {
+    if (message.slice(0, 1) === "f") {
+      for (let i = 0; i < 1; ) {
+        const firstLetter = message.slice(i, i + 1).toUpperCase();
+        console.log(firstLetter);
+        i++;
+      }
+      for (let i = 1; i < message.length; ) {
+        console.log(message.slice(i, i + 1));
+        i++;
+      }
+    }
+    if (message.slice(0, 1) !== "f") {
+      for (let i = 0; i < message.length; ) {
+        console.log(message.slice(i, i + 1));
+        i++;
+        //console.log(i);
+      }
+    }
+  } else {
+    if (message.slice(0, 1) === "f") {
+      for (let j = 0; j < 1; ) {
+        const firstLetter = message.slice(j, j + 1).toUpperCase();
+        console.log(firstLetter);
+        j++;
+      }
+      for (let j = 1; j < 10; ) {
+        console.log(message.slice(j, j + 1));
+        j++;
+      }
+    } else {
+      for (let j = 0; j < 10; ) {
+        console.log(message.slice(j, j + 1));
+        j++;
+        //console.log(i);
+      }
+    }
+  }
+}
+
+showVerticalMessage("fronhuopgh");
